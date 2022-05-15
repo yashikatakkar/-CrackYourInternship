4dd69dd621f08415f944a3955c1390a0261ce050
Yashika Takkar <76724146+yashikatakkar@users.noreply.github.com>
2022-05-15 23:18:10 +0530

    Create find_all_duplicates_in_an_array.cpp

---
+++ arrays/find_all_duplicates_in_an_array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+       vector <int>arr;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                arr.push_back(nums[i]);
+            }
+        }
+        return arr;
+    }
+};
