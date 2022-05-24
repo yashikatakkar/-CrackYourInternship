07db9a5e331a6a47075b4b3fef917b6511610516
Yashika Takkar <76724146+yashikatakkar@users.noreply.github.com>
2022-05-24 23:41:06 +0530

    Create pair_with_given_difference.cpp

---
+++ sorting and searching/pair_with_given_difference.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool findPair(int arr[], int size, int n)
+{
+	// Initialize positions of two elements
+	int i = 0;
+	int j = 1;
+
+	// Search for a pair
+	while (i < size && j < size)
+	{
+		if (i != j && (arr[j] - arr[i] == n || arr[i] - arr[j] == n) )
+		{
+			cout << "Pair Found: (" << arr[i] <<
+						", " << arr[j] << ")";
+			return true;
+		}
+		else if (arr[j]-arr[i] < n)
+			j++;
+		else
+			i++;
+	}
+
+	cout << "No such pair";
+	return false;
+}
